40d85ed673fe01907c150a4bf50429ed6a3e7a40
Ankit-Nag <ankitknag@gmail.com>
2021-01-06 00:37:57 +0530

    v1.2 Style update
    style: Animations, transitions, status bar,
    
    !Output div to be styled!

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var btnPaid = document.querySelector("#btn-billPaid");
 var inputBill = document.querySelector("#billAmt");
 var inputPaid = document.querySelector("#billPaid");
 var output = document.querySelector(".output-box");
+var statusBar = document.querySelector(".status-bar");
+
 var billAmount = 0,
     billPaid = 0;
 var notes = [{
@@ -37,8 +39,68 @@ var notes = [{
 
 function billEventHandler() {
     billAmount = inputBill.value;
-    output.innerText="";
-    if (billAmount === "") {
+    output.innerText = "";
+
+}
+
+function paidEventHandler() {
+    billPaid = inputPaid.value;
+    var change = billPaid - billAmount;
+    if (change > 0) {
+        calculateNotes(change);
+        statusBar.style.borderColor = "var(--light-grey)";
+        output.innerText = "";
+        for (var i = 0; i < notes.length; i++) {
+            if (notes[i].number > 0) {
+                output.innerText += notes[i].name + " : " + notes[i].number + "\n";
+            }
+        }
+    } else if (change === 0) {
+        output.innerText = "Thank you for shopping with us!"
+        statusBar.style.borderColor = "var(--green)";
+    } else {
+        output.innerText = "Please pay ₹" + Math.abs(change) + " more";
+        statusBar.style.borderColor = "var(--red)";
+    }
+}
+
+function calculateNotes(change) {
+    console.log(change);
+    for (var i = 0; change > 0; i++) {
+        if (change >= 2000) {
+            change -= 2000;
+            notes[0].number++;
+            console.log(notes[0].number);
+        } else if (change >= 500) {
+            change -= 500;
+            notes[1].number++;
+            console.log(notes[1].number);
+        } else if (change >= 100) {
+            change -= 100;
+            notes[2].number++;
+            console.log(notes[2].number);
+        } else if (change >= 20) {
+            change -= 20;
+            notes[3].number++;
+            console.log(notes[3].number);
+        } else if (change >= 10) {
+            change -= 10;
+            notes[4].number++;
+            console.log(notes[4].number);
+        } else if (change >= 5) {
+            change -= 5;
+            notes[5].number++;
+            console.log(notes[5].number);
+        } else if (change >= 1) {
+            change -= 1;
+            notes[6].number++;
+            console.log(notes[6].number);
+        }
+    }
+}
+
+function disableHandler() {
+    if (inputBill.value == "") {
         inputPaid.disabled = true;
         btnPaid.disabled = true;
     } else {
@@ -47,63 +109,6 @@ function billEventHandler() {
     }
 }
 
-function paidEventHandler() {
-    billPaid = inputPaid.value;
-    var change = billPaid - billAmount;
-    if(change>0){
-    calculateNotes(change);
-    for(var i=0;i<notes.length;i++){
-        output.innerText+=notes[i].name + " " + notes[i].number+"\n";
-    }
-    }
-    else if(change===0){
-        output.innerText="Thank you for shopping with us!"
-    }
-    else{
-        output.innerText="Please pay ₹" + Math.abs(change) +" more";
-    }
-}
-
-function calculateNotes(change) {
-    console.log(change);
-    for(var i=0;change>0;i++){
-        if(change>=2000){
-            change-=2000;
-            notes[0].number++;
-            console.log(notes[0].number);
-        }
-        else if(change>=500){
-            change-=500;
-            notes[1].number++;
-            console.log(notes[1].number);
-        }
-        else if(change>=100){
-            change-=100;
-            notes[2].number++;
-            console.log(notes[2].number);
-        }
-        else if(change>=20){
-            change-=20;
-            notes[3].number++;
-            console.log(notes[3].number);
-        }
-        else if(change>=10){
-            change-=10;
-            notes[4].number++;
-            console.log(notes[4].number);
-        }
-        else if(change>=5){
-            change-=5;
-            notes[5].number++;
-            console.log(notes[5].number);
-        }
-        else if(change>=1){
-            change-=1;
-            notes[6].number++;
-            console.log(notes[6].number);
-        }
-    }
-}
-
+inputBill.addEventListener("input", disableHandler);
 btnBill.addEventListener("click", billEventHandler);
 btnPaid.addEventListener("click", paidEventHandler);
